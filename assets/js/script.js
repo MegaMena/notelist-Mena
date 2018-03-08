fb1f7c0fcfa3dcf3b9b344769d629d2914565e3b
MegaMena <guldsmil@hotmail.com>
2018-03-08 10:40:42 +0100

    storage of data

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+console.log("ladida");
+
+function setLocal(localNotes)
+{
+  var jsonData = JSON.stringify(localNotes);
+
+  localStorage.setItem("notes", jsonData);
+}
+
+function getLocal()
+{
+  var notes = localStorage.getItem("notes");
+
+  if(notes == null)
+  {
+    return [];
+  }
+  else {
+    return JSON.parse(notes);
+  }
+}
+
+function submitNote()
+{
+  var origNotes = getLocal();
+
+  var newNote = {
+    text: "Ladida",
+    dueDate: "2018-03-09 13:00:00",
+    important: true
+  };
+
+  origNotes.push(newNote);
+
+  setLocal(origNotes);
+}
+
+submitNote();
